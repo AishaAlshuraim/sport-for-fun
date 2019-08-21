1e8ec1430e4416c33629f596ba829b68d05e2721
aisha <aishaalshuraim@gmail.com>
2019-08-21 16:04:27 +0300

    Updates

---
+++ precache-manifest.fa1739ba8d947d09448047bd01784ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8e0c151025a539108a28e94e356f71",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "cab2e79b842f428ec924",
+    "url": "/repository-name/static/css/2.47fb9b64.chunk.css"
+  },
+  {
+    "revision": "5e10b9b92f96268f76aa",
+    "url": "/repository-name/static/css/main.d00a5523.chunk.css"
+  },
+  {
+    "revision": "cab2e79b842f428ec924",
+    "url": "/repository-name/static/js/2.1bc56f81.chunk.js"
+  },
+  {
+    "revision": "5e10b9b92f96268f76aa",
+    "url": "/repository-name/static/js/main.bf654a6e.chunk.js"
+  },
+  {
+    "revision": "d7fefbdfb147e20dd8fe",
+    "url": "/repository-name/static/js/runtime~main.cd4299c2.js"
+  }
+]);
